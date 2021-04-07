16e3fbd25a4f288d625adbe132abb1f043411aaa
Phuong Bui <51000027+censodev@users.noreply.github.com>
2021-04-07 14:45:04 +0700

    config prod

--- ui/src/environments/environment.prod.ts
+++ ui/src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
-  wsEndpoint: 'ws://20.198.99.49:8000/ws',
-  apiEndpoint: 'http://20.198.99.49:8000/api',
+  wsEndpoint: 'ws://20.198.102.32:8000/ws',
+  apiEndpoint: 'http://20.198.102.32:8000/api',
 };
